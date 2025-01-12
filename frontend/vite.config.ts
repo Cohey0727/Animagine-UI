b78761ecd0e077fc121603ade2be4ffb1215133f
KoheiOkamoto <ohayousagi.ac.kook0727@gmail.com>
2025-01-12 12:25:39 +0900

    main

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { reactRouter } from "@react-router/dev/vite";
+import wyw from "@wyw-in-js/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    wyw({
+      include: ["**/*.{ts,tsx}"],
+      babelOptions: {
+        presets: ["@babel/preset-typescript", "@babel/preset-react"],
+      },
+    }),
+  ],
+});
